93adc484581d2ca40920f96aab0491754d1d1239
sonnenstrahl <david.haas@uzh.ch>
2018-01-25 07:01:02 +0100

    2nd commit

--- static/js/charts.js
+++ static/js/charts.js
@@ -555,12 +555,11 @@ function buildVisualisation(e, json, geoJson, params) {
             for (var k in obj){
                 var key = concatenateNumber(v['day'], +k);
                 if (!p[key])
-                    p[key] = {0: key, 1: 1};
+                    p[key] = {0: obj[+k], 1: 1};
                 else {
-                    p[key][0] += obj[k];
+                    p[key][0] += obj[+k];
                     p[key][1] += 1;
                 }
-
             }
             return p;
         }
@@ -576,7 +575,7 @@ function buildVisualisation(e, json, geoJson, params) {
             for (var k in obj){
                 var key = concatenateNumber(v['day'], +k);
                 p[key][1] -= 1;
-                p[key][0] -= obj[k];
+                p[key][0] -= obj[+k];
 
             }
             return p;
@@ -637,7 +636,7 @@ function buildVisualisation(e, json, geoJson, params) {
             .rows([2, 3, 4, 5, 6, 7, 1])
             .cols([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
                 , 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69])
-            .colors(d3.scale.linear().range(['white', 'darkgreen']))
+            .colors(d3.scale.linear().range(['white', 'red']))
             .title(function (d) {
                 return 'Avg: ' + (d.avg);
             })
